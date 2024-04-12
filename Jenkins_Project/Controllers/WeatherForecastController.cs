ebac2221cef1793404ed737ca71a35cf6048da86
Neha20192 <160131659+Neha20192@users.noreply.github.com>
2024-04-12 11:12:54 +0530

    Update WeatherForecastController.cs

--- Jenkins_Project/Controllers/WeatherForecastController.cs
+++ Jenkins_Project/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace Jenkins_Project.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "very cool"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
